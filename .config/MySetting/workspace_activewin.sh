aaf769a834f3636812172aca611b5df081a7e7d3
Rais Zainuri <raiszainuri09@gmail.com>
2021-02-13 12:20:27 +0700

    add net usage

--- .config/MySetting/workspace_activewin.sh
+++ .config/MySetting/workspace_activewin.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 #date_time=$(date +%_d.%m.%H.%M.%S)
+netusage=$(vnstat | grep -w 'today' | awk '{print $8 $9}')
 curr_workspace=$(wmctrl -d | grep -w $5 '*' | awk '{print $10}')
 active_win=$(xdotool getwindowfocus getwindowname)
-printf " ␥ $curr_workspace ␥ $active_win"
+printf " $netusage ␥ $curr_workspace ␥ $active_win"
