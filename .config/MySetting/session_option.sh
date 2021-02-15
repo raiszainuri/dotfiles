f9589c9d2c52bca52db068c337f2f89d33142111
Rais Zainuri <raiszainuri09@gmail.com>
2021-02-15 23:54:44 +0700

    config screenkey, config session option

--- .config/MySetting/session_option.sh
+++ .config/MySetting/session_option.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 while true; do
-    read -p "Shutdown atau Reboot? [r/s]
+    read -p "Shutdown atanapi Reboot? [r/s]
 " yn
     case $yn in
         [Ss]* ) shutdown now; break;;
         [Rr]* ) reboot;;
+        [muhun]* ) shutdown now;;
         * ) exit;;
     esac
 done
