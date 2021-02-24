e4d6412bc273174a85a26d7704b403c30597bc05
Rais Zainuri <raiszainuri09@gmail.com>
2021-02-24 11:49:21 +0700

    get temp

--- .config/MySetting/get_temp.sh
+++ .config/MySetting/get_temp.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-
-currenttemp=$(sensors | grep -w 'temp1' | awk '{print $2}' | grep '+' -n)
-echo "$currenttemp "
+currentcpu=$(sensors | grep -w 'temp1' | awk '{print $2}' | sed -n 2p | cut -d '+' -f 2)
+echo "$currentcpu"
