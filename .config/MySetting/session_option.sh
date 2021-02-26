49c75647623c93aa0a98500bb2978adc57a87f23
Rais Zainuri <raiszainuri09@gmail.com>
2021-02-26 15:09:32 +0700

    polybar xfce, session option, config polybar

--- .config/MySetting/session_option.sh
+++ .config/MySetting/session_option.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 while true; do
-    read -p "Shutdown atanapi Reboot? [r/s]
+    read -p "Shutdown atanapi Reboot? [s/r]
 " yn
     case $yn in
         [Ss]* ) shutdown now; break;;
         [Rr]* ) reboot;;
-        [muhun]* ) shutdown now;;
         * ) exit;;
     esac
 done
