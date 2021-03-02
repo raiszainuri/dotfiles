3152acbf7d83e3723ca3f2c134bf419cdc67706e
Rais Zainuri <raiszainuri09@gmail.com>
2021-03-02 16:28:49 +0700

    current workspace index from 1

--- .config/MySetting/current_workspace.sh
+++ .config/MySetting/current_workspace.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 #date_time=$(date +%_d.%m.%H.%M.%S)
-curr_workspace=$(wmctrl -d | grep -w $1 '*' | awk '{print $1}')
+curr_workspace=$(wmctrl -d | grep -w $1 '*' | awk '{print $10}')
 case $curr_workspace in
-    [0]* ) printf "°1  2  3  4  5  6  7  8  9  10 ";;
-    [1]* ) printf " 1 °2  3  4  5  6  7  8  9  10 ";;
-    [2]* ) printf " 1  2 °3  4  5  6  7  8  9  10 ";;
-    [3]* ) printf " 1  2  3 °4  5  6  7  8  9  10 ";;
-    [4]* ) printf " 1  2  3  4 °5  6  7  8  9  10 ";;
-    [5]* ) printf " 1  2  3  4  5 °6  7  8  9  10 ";;
-    [6]* ) printf " 1  2  3  4  5  6 °7  8  9  10 ";;
-    [7]* ) printf " 1  2  3  4  5  6  7 °8  9  10 ";;
-    [8]* ) printf " 1  2  3  4  5  6  7  8 °9  10 ";;
-    [9]* ) printf " 1  2  3  4  5  6  7  8  9 °10 ";;
+    [1] ) printf "°1  2  3  4  5  6  7  8  9  10 ";;
+    [2] ) printf " 1 °2  3  4  5  6  7  8  9  10 ";;
+    [3] ) printf " 1  2 °3  4  5  6  7  8  9  10 ";;
+    [4] ) printf " 1  2  3 °4  5  6  7  8  9  10 ";;
+    [5] ) printf " 1  2  3  4 °5  6  7  8  9  10 ";;
+    [6] ) printf " 1  2  3  4  5 °6  7  8  9  10 ";;
+    [7] ) printf " 1  2  3  4  5  6 °7  8  9  10 ";;
+    [8] ) printf " 1  2  3  4  5  6  7 °8  9  10 ";;
+    [9] ) printf " 1  2  3  4  5  6  7  8 °9  10 ";;
+    *   ) printf " 1  2  3  4  5  6  7  8  9 °10 ";;
 esac
