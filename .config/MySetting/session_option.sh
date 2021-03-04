4f35c72490d7ab6e29f0126340d0b60127c2e462
Rais Zainuri <raiszainuri09@gmail.com>
2021-03-04 20:43:29 +0700

    picom, i3, i3lock

--- .config/MySetting/session_option.sh
+++ .config/MySetting/session_option.sh
@@ -3,7 +3,7 @@ while true; do
     read -p "shutdown? [y/g]
 " yn
     case $yn in
-        [y]* ) shutdown now; break;;
+        [ys]* ) shutdown now; break;;
         [r]* ) reboot;;
         * ) exit;;
     esac
