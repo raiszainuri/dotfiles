a4408fca873ed9529bdf34c5633248895078c89d
Rais Zainuri <raiszainuri09@gmail.com>
2021-03-28 06:23:11 +0700

    commit

--- .config/MySetting/current_workspace.sh
+++ .config/MySetting/current_workspace.sh
@@ -3,14 +3,14 @@
 #date_time=$(date +%_d.%m.%H.%M.%S)
 curr_workspace=$(wmctrl -d | grep -w $1 '*' | awk '{print $10}')
 case $curr_workspace in
-    [1] ) printf "°1  2  3  4  5  6  7  8  9  10 ";;
-    [2] ) printf " 1 °2  3  4  5  6  7  8  9  10 ";;
-    [3] ) printf " 1  2 °3  4  5  6  7  8  9  10 ";;
-    [4] ) printf " 1  2  3 °4  5  6  7  8  9  10 ";;
-    [5] ) printf " 1  2  3  4 °5  6  7  8  9  10 ";;
-    [6] ) printf " 1  2  3  4  5 °6  7  8  9  10 ";;
-    [7] ) printf " 1  2  3  4  5  6 °7  8  9  10 ";;
-    [8] ) printf " 1  2  3  4  5  6  7 °8  9  10 ";;
-    [9] ) printf " 1  2  3  4  5  6  7  8 °9  10 ";;
-    *   ) printf " 1  2  3  4  5  6  7  8  9 °10 ";;
+    [1] ) printf "°1  2  3  4  7  8  9  0 ";;
+    [2] ) printf " 1 °2  3  4  7  8  9  0 ";;
+    [3] ) printf " 1  2 °3  4  7  8  9  0 ";;
+    [4] ) printf " 1  2  3 °4  7  8  9  0 ";;
+    [5] ) printf " 2  3  4 °5  6  7  8  9 ";;
+    [6] ) printf " 2  3  4  5 °6  7  8  9 ";;
+    [7] ) printf " 1  2  3  4 °7  8  9  0 ";;
+    [8] ) printf " 1  2  3  4  7 °8  9  0 ";;
+    [9] ) printf " 1  2  3  4  7  8 °9  0 ";;
+    *   ) printf " 1  2  3  4  7  8  9 °0 ";;
 esac
