2dd135d6d1c7df972ef882ac27e5d27c9e4c4d67
Rais Zainuri <raiszainuri09@gmail.com>
2021-04-28 09:04:51 +0700

    ...

--- .config/MySetting/workspace_activewin.sh
+++ .config/MySetting/workspace_activewin.sh
@@ -4,5 +4,13 @@
 #netusage=$(vnstat | grep -w 'today' | awk '{print $8 $9}')
 #curr_workspace=$(wmctrl -d | grep -w $5 '*' | awk '{print $10}')
 active_win=$(xdotool getwindowfocus getwindowname)
+length_str=${#active_win}
 #printf " $netusage ␥ $curr_workspace ␥ $active_win"
-printf " => ${active_win:0:40}..."
+if  (( $length_str >= 45 ))
+then
+    echo "<txt> => ${active_win:0:45}...</txt>"
+else
+    echo "<txt> => $active_win</txt>"
+fi
+
+echo "<tool>$active_win</tool>"
