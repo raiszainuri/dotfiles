b07ae8c15ce5f7520d18a8d0633fe6e0420d752d
Rais Zainuri <raiszainuri09@gmail.com>
2021-05-04 07:04:07 +0700

    stat net

--- .config/MySetting/stat_network.sh
+++ .config/MySetting/stat_network.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 netusage=$(vnstat | grep -w 'today' | awk '{print $8 $9}')
-netusage_h=$(vnstat -h 4 | cut -d " " -f 6-)
+netusage_h=$(vnstat -h 8 | cut -d " " -f 6-)
 #netusage=$(netstat -i wlp1s0 | grep -w 'wlp1s0' | awk '{print $3+$7}')
 #total_stat=$(( $netusage / 1000 ))
 echo "<txt> $netusage |</txt>"
