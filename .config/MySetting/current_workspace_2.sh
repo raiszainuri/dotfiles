a435917c795fe697c93130b7063ba545b28cbb0a
Rais Zainuri <raiszainuri09@gmail.com>
2021-05-15 20:15:07 +0700

    current workspace

---
+++ .config/MySetting/current_workspace_2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#date_time=$(date +%_d.%m.%H.%M.%S)
+curr_workspace=$(wmctrl -d | grep -w $1 '*' | awk '{print $10}')
+case $curr_workspace in
+    [1] ) printf "1";;
+    [2] ) printf "2";;
+    [3] ) printf "3";;
+    [4] ) printf "4";;
+    [5] ) printf "5";;
+    [6] ) printf "6";;
+    [7] ) printf "7";;
+    [8] ) printf "8";;
+    [9] ) printf "9";;
+    *   ) printf "10";;
+esac
