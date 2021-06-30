c5258ff8031a2e907b87efc4f810e2e102a0faea
Rais Zainuri <raiszainuri09@gmail.com>
2021-06-30 11:14:56 +0700

    new

---
+++ .config/MySetting/prog_start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sleep 3
+ffplay -nodisp -autoexit -loglevel quiet ~/.config/MySetting/startup.mp3
--- .config/glava/circle.glsl
+++ .config/glava/circle.glsl
@@ -1,9 +1,9 @@
 /* center radius (pixels) */
-#define C_RADIUS 128
+#define C_RADIUS 140
 /* center line thickness (pixels) */
-#define C_LINE 1.5
+#define C_LINE 3
 /* outline color */
-#define OUTLINE #333333
+#define OUTLINE #B3B3B3
 /* Amplify magnitude of the results each bar displays */
 #define AMPLIFY 150
 /* Angle (in radians) for how much to rotate the visualizer */
